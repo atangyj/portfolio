b830a349139f4910d5c3f55a4d5533f932cd4dda
tyj <atangyj@gmail.com>
2019-05-21 00:40:28 +0100

    add hero project

--- src/index.js
+++ src/index.js
@@ -1,25 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import drumMachine from './img/drumMachine.png';
-import localWeather from './img/localWeather.png';
-import pomodoro from './img/pomodoro.png';
-import randomQuote from './img/randomQuote.png';
-import twitch from './img/twitch.png';
-import wikipedia from './img/wikipedia.png';
-import CV from './TYJ_CV_2018_FE_v2.pdf';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import drumMachine from "./img/drumMachine.png";
+import localWeather from "./img/localWeather.png";
+import pomodoro from "./img/pomodoro.png";
+import randomQuote from "./img/randomQuote.png";
+import twitch from "./img/twitch.png";
+import wikipedia from "./img/wikipedia.png";
+import hero from "./img/hero.png";
+import CV from "./AliceTang_CV_2019_FE.pdf";
 
 const projects = [
-   {
+  {
+    name: "You can be hero",
+    src: hero,
+    intro: "What kind of superhero do you want to be?",
+    skill: ["Javascript ES6", "React"],
+    href: "/doopollheroes/index.html"
+  },
+  {
     name: "The Path to this world",
     src: wikipedia,
-    intro:"Knowledge is the path to this world. The path has no end. Wikipedia is the entry. This newly-desiged wikipedia interface makes your searching journey easier.",
-    skill: ["Javascript ES6","Wikipedia API"],
+    intro:
+      "Knowledge is the path to this world. The path has no end. Wikipedia is the entry. This newly-desiged wikipedia interface makes your searching journey easier.",
+    skill: ["Javascript ES6", "Wikipedia API"],
     href: "/the-path-to-the-world/index.html"
   },
 
-   {
+  {
     name: "Shakespeare's twitter",
     src: randomQuote,
     intro: "If Shakespeare has a twitter account, how will it look like?",
@@ -27,34 +36,38 @@ const projects = [
     href: "/shakespeares-twitter/index.html"
   },
 
- {
+  {
     name: "Whiplash",
     src: drumMachine,
-    intro:'Do you like the movie "Whiplash"? This application lets you play drums by your keyboard!',
+    intro:
+      'Do you like the movie "Whiplash"? This application lets you play drums by your keyboard!',
     skill: ["React"],
     href: "/whiplash/index.html"
   },
 
-   {
+  {
     name: "Love rival",
     src: twitch,
-    intro:"My husband watches live stream on Twitch every day. I built a website which collects his favorite streamers.",
+    intro:
+      "My husband watches live stream on Twitch every day. I built a website which collects his favorite streamers.",
     skill: ["Javascript ES6", "Twitch API"],
     href: "/love-rival/index.html"
   },
 
-   {
+  {
     name: "Londoner's everyday topic",
     src: localWeather,
-    intro:"Feel difficult to initiate a conversation? Check London's weather on this website.",
+    intro:
+      "Feel difficult to initiate a conversation? Check London's weather on this website.",
     skill: ["Javascript ES6", "Weather API"],
     href: "/londoner-everyday-topic/index.html"
   },
 
-   {
+  {
     name: "Fitness timer",
     src: pomodoro,
-    intro:"I go to workout studio every weekdays. I built a fitness timer for myself, so I can train myself at home.",
+    intro:
+      "I go to workout studio every weekdays. I built a fitness timer for myself, so I can train myself at home.",
     skill: ["React"],
     href: "/fitness-timer"
   }
@@ -71,12 +84,18 @@ const projects = [
 // }
 
 class Project extends React.Component {
-  render(){
+  render() {
     const skills = this.props.skill;
-    const skill_list = skills.map((skill,idx)=> <span key={idx.toString()}>{skill}</span>);
-    return(
+    const skill_list = skills.map((skill, idx) => (
+      <span key={idx.toString()}>{skill}</span>
+    ));
+    return (
       <div className="container-list-project">
-        <img className="project-screenshot" alt={this.props.name} src={this.props.src} />
+        <img
+          className="project-screenshot"
+          alt={this.props.name}
+          src={this.props.src}
+        />
         <div className="container-project-desc">
           <div className="top">
             <h1>{this.props.name}</h1>
@@ -85,84 +104,142 @@ class Project extends React.Component {
 
           <div className="bottom">
             <p>{this.props.intro}</p>
-            <a className="link-project" href={this.props.href} target="_blank" rel="noopener noreferrer">visit website</a>
+            <a
+              className="link-project"
+              href={this.props.href}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              visit website
+            </a>
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
-
 class Portfolio extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.renderProject = this.renderProject.bind(this);
     this.clickNav = this.clickNav.bind(this);
-
   }
 
-  renderProject(i){
-    const link = process.env.NODE_ENV === "development" ? "https://atangyj.github.io" + projects[i].href : projects[i].href;
+  renderProject(i) {
+    const link =
+      process.env.NODE_ENV === "development"
+        ? "https://atangyj.github.io" + projects[i].href
+        : projects[i].href;
     return (
-      <Project key={i.toString()} name = {projects[i].name} src={projects[i].src} intro={projects[i].intro} skill={projects[i].skill} href={link}/>
-    )
+      <Project
+        key={i.toString()}
+        name={projects[i].name}
+        src={projects[i].src}
+        intro={projects[i].intro}
+        skill={projects[i].skill}
+        href={link}
+      />
+    );
   }
 
+  clickNav(e) {
+    const name = e.target.classList.value;
+    const ele = document.querySelector(".container-" + name);
 
-  clickNav(e){
-      const name = e.target.classList.value;
-      const ele = document.querySelector(".container-"+name);
-
-      ele.scrollIntoView();
-      ele.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+    ele.scrollIntoView();
+    ele.scrollIntoView({
+      behavior: "smooth",
+      block: "start",
+      inline: "nearest"
+    });
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="container">
         <header>
-
           <h1 className="header-title">Hi, I'm Alice.</h1>
-          <p>I'm a London-based web developer and I bring easy-to-use interface to the daily life by JavaScript and React. I'd managed and designed a phone number identification App at <a target="_blank" href="https://whoscall.com/en-US/" rel="noopener noreferrer">Gogolook</a>. I'd also researched decision-making behavior at <a target="_blank" href="http://gibms.mc.ntu.edu.tw/Eng/" rel="noopener noreferrer">Brain & Mind Lab</a>. Welcome to look at my projects.</p>
+          <p>
+            I'm a London-based web developer and I bring easy-to-use interface
+            to the daily life by JavaScript and React. I'd managed and designed
+            a phone number identification App at{" "}
+            <a
+              target="_blank"
+              href="https://whoscall.com/en-US/"
+              rel="noopener noreferrer"
+            >
+              Gogolook
+            </a>
+            . I'd also researched decision-making behavior at{" "}
+            <a
+              target="_blank"
+              href="http://gibms.mc.ntu.edu.tw/Eng/"
+              rel="noopener noreferrer"
+            >
+              Brain & Mind Lab
+            </a>
+            . Welcome to look at my projects.
+          </p>
 
           <nav className="clear">
             <ul>
-              <li className="projects" onClick={this.clickNav}>Project</li>
-              <li><a className="link-resume" href={CV} target="_blank" rel="noopener noreferrer">Resume</a></li>
-              <li className="contact" onClick={this.clickNav}>Contact</li>
+              <li className="projects" onClick={this.clickNav}>
+                Project
+              </li>
+              <li>
+                <a
+                  className="link-resume"
+                  href={CV}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Resume
+                </a>
+              </li>
+              <li className="contact" onClick={this.clickNav}>
+                Contact
+              </li>
             </ul>
           </nav>
-
         </header>
 
-
         <div className="container-projects">
-            {this.renderProject(0)}
-            {this.renderProject(1)}
-            {this.renderProject(2)}
-            {this.renderProject(3)}
-            {this.renderProject(4)}
-            {this.renderProject(5)}
+          {this.renderProject(0)}
+          {this.renderProject(1)}
+          {this.renderProject(2)}
+          {this.renderProject(3)}
+          {this.renderProject(4)}
+          {this.renderProject(5)}
+          {this.renderProject(6)}
         </div>
         <div className="container-contact">
-          <h1>Welcom to say hi to me at <a href="mailto:atangyj@gmail.com">atangyj@gmail.com</a></h1>
-          <h1>Find me on <a href="www.linkedin.com/in/aliceYJ">LinkedIn</a></h1>
-          <h1>View my code on <a href="https://github.com/atangyj">github</a></h1>
+          <h1>
+            Welcom to say hi to me at{" "}
+            <a href="mailto:atangyj@gmail.com">atangyj@gmail.com</a>
+          </h1>
+          <h1>
+            Find me on <a href="www.linkedin.com/in/aliceYJ">LinkedIn</a>
+          </h1>
+          <h1>
+            View my code on <a href="https://github.com/atangyj">github</a>
+          </h1>
         </div>
       </div>
-    )
+    );
   }
 }
 
-ReactDOM.render(<Portfolio />, document.getElementById('root'));
+ReactDOM.render(<Portfolio />, document.getElementById("root"));
 
-window.onscroll = function(){fixHeader()};
+window.onscroll = function() {
+  fixHeader();
+};
 
-const nav = document.querySelector('nav');
+const nav = document.querySelector("nav");
 const sticky = nav.offsetTop;
 
-function fixHeader(){
+function fixHeader() {
   if (window.pageYOffset > sticky) {
     nav.classList.add("sticky");
   } else {
